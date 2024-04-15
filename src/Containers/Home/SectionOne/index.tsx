469a9f0f129108a1c80d5bd99524b61c4bc91418
emanuel caro <darkemlord@gmail.com>
2024-04-15 23:03:09 +0900

    refs #1 adding ts config paths and refactoring

---
+++ src/Containers/Home/SectionOne/index.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import {
+  bannerContainer,
+  imageContainer,
+  bannerSectionImage,
+  bannerTextContainer,
+  bannerTextBackground,
+  BannerText,
+} from "../homeStyles";
+import homeBackground from "@assets/images/png/banner-circle-image.png";
+
+const SectionOne: React.FC = () => {
+  return (
+    <Box sx={bannerContainer}>
+      <Box sx={imageContainer}>
+        <Box sx={bannerSectionImage} component="img" src={homeBackground}></Box>
+        <Box sx={bannerTextContainer}>
+          <Box sx={bannerTextBackground}>
+            <Typography sx={BannerText}>
+              Exploring Horizons: Ideas, Innovation,
+              <br />
+              and Inspiration for the Modern World.
+              <br /> Welcome to our space for discovery and <br />
+              dialogue!
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default SectionOne;
--- src/Containers/Home/index.tsx
+++ src/Containers/Home/index.tsx
@@ -1,34 +1,9 @@
-import { Box, Typography } from "@mui/material";
+// import { Box } from "@mui/material";
 import React from "react";
-import {
-  BannerText,
-  bannerTextContainer,
-  bannerContainer,
-  bannerSectionImage,
-  imageContainer,
-  bannerTextBackground,
-} from "./homeStyles";
-import homeBackground from "../../assets/images/png/banner-circle-image.png";
+import SectionOne from "./SectionOne";
 
 const HomeContainer: React.FC = () => {
-  return (
-    <Box sx={bannerContainer}>
-      <Box sx={imageContainer}>
-        <Box sx={bannerSectionImage} component="img" src={homeBackground}></Box>
-        <Box sx={bannerTextContainer}>
-          <Box sx={bannerTextBackground}>
-            <Typography sx={BannerText}>
-              Exploring Horizons: Ideas, Innovation,
-              <br />
-              and Inspiration for the Modern World.
-              <br /> Welcome to our space for discovery and <br />
-              dialogue!
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
-  );
+  return <SectionOne />;
 };
 
 export default HomeContainer;
